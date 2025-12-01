cdc302a911938e509dc9503fc37b21d680f4c8dd
Dots <dotvnl@gmail.com>
2025-12-01 18:54:11 +0200

    13881

--- csgowatchdog/csgowatchdog.sh
+++ csgowatchdog/csgowatchdog.sh
@@ -5,21 +5,17 @@ set -ueEo pipefail
 update_csgo() {
     # Download CS:GO to /watchdog/csgo/install
     HOME="/watchdog/steamcmd" /watchdog/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +@bMetricsEnabled 0 +force_install_dir "/watchdog/csgo/install" +login anonymous +app_update 740 validate +quit 1>&2
-
-    # Check which version we just installed
-    local installed_version="$(grep PatchVersion= "/watchdog/csgo/install/game/csgo/steam.inf" | tr -cd "0-9")"
-
-    # Return if we already have this version
-    [ ! -d "/watchdog/csgo/builds/$installed_version" ]
+    # Return if we already have csgo build 13881
+    [ -d "/watchdog/csgo/builds/13881" ] && return 0
 
     # Hard symlink the files from /watchdog/csgo/install to /watchdog/.tmp, then rename /watchdog/.tmp to /watchdog/csgo/builds/????? so it's atomic.
     # Must use a tmp directory inside of the /watchdog because symlinks don't work across filesystems.
     cp -rl "/watchdog/csgo/install" "/watchdog/.tmp"
-    mv "/watchdog/.tmp" "/watchdog/csgo/builds/$installed_version"
+    mv "/watchdog/.tmp" "/watchdog/csgo/builds/13881"
 
     # Store the version in latest.txt so servers can detect an update
     rm "/tmp/latest.txt"
-    echo "$installed_version" > "/tmp/latest.txt"
+    echo "13881" > "/tmp/latest.txt"
     mv "/tmp/latest.txt" "/watchdog/csgo/latest.txt"
 }
 
@@ -37,7 +33,7 @@ for (( first=1;; first=0 )); do
     # The temporary directory might exist if update_csgo fails
     rm -rf "/watchdog/.tmp"
 
-    latest_version="1.38.8.1" || continue
+    latest_version="13881" || continue
 
     # Remove outdated CS:GO builds that are not being used by any server
     find "/watchdog/csgo/builds" -mindepth 1 -maxdepth 1 -type d ! -name "$latest_version" -exec flock -nx "{}/.lockfile" --command "rm -rf \"{}\"" \; || true
