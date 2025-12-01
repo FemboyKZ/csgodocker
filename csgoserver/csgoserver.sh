ee5e59247113b3fa87b33df10dfa0fd9e927c627
Dots <dotvnl@gmail.com>
2025-12-01 18:53:34 +0200

    make servers work without running watchdog

--- csgoserver/csgoserver.sh
+++ csgoserver/csgoserver.sh
@@ -8,9 +8,11 @@ mkdir -p "$server_dir"
 for (( first=1;; first=0 )); do
     [ $first -eq 0 ] && sleep 10
 
-    build_ver="0"
-    build_dir="/csgo"
-    [ -d "$build_dir" ] || exit 1
+    # Wait for watchdog to provide latest version
+    [ -f "/watchdog/csgo/latest.txt" ] || continue
+    build_ver="$(cat /watchdog/csgo/latest.txt)"
+    build_dir="/watchdog/csgo/builds/$build_ver"
+    [ -d "$build_dir" ] || continue
 
     rm -rf "$server_dir"/*
     LD_LIBRARY_PATH="$server_dir:$server_dir/bin" HOME="/tmp/csgohome" root="" build_ver="$build_ver" build_dir="$build_dir" server_dir="$server_dir" /bin/bash /user/run.sh
